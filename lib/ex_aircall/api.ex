a673d3e5d19c955861bfb6e5cf31ed1a093c7d03
Erik Nilsen <enilsen16@live.com>
2016-08-30 18:39:19 -0700

    update documentation [ci skip]

--- lib/ex_aircall/api.ex
+++ lib/ex_aircall/api.ex
@@ -1,14 +1,23 @@
 defmodule ExAircall.API do
+  @moduledoc """
+    API Endpoint wrapper
+  """
+  
   @base_url "api.aircall.io/v1"
   @api_token Application.get_env(:ex_aircall, :api_token)
   @app_id Application.get_env(:ex_aircall, :app_id)
 
-
+  @doc """
+  Handy function to test api status or check for valid credentials
+  """
   def ping do
     url = "https://#{@app_id}:#{@api_token}@#{@base_url}/ping"
     HTTPoison.get! url
   end
 
+  @doc """
+  Calls endpoint. Returns struct similar to http://developer.aircall.io/#call
+  """
   def call do
     url = "https://#{@app_id}:#{@api_token}@#{@base_url}/calls"
     HTTPoison.get! url
