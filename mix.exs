0638461a97ac170fe83c790a4b20f1cbf2c1273e
Erik Nilsen <enilsen16@live.com>
2016-09-03 14:12:18 -0700

    increment version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExAircall.Mixfile do
 
   def project do
     [app: :ex_aircall,
-     version: "0.3.0",
+     version: "0.3.1",
      elixir: "~> 1.3",
      docs: docs(),
      description: description(),
