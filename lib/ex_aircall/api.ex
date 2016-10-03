6dc4de3560498e7603822993754a8468e6ff7ac2
Erik Nilsen <enilsen16@live.com>
2016-10-03 11:36:45 -0700

    remove module attributes in favor of private functions

--- lib/ex_aircall/api.ex
+++ lib/ex_aircall/api.ex
@@ -4,15 +4,12 @@ defmodule ExAircall.API do
   """
 
   @base_url "api.aircall.io/v1"
-  @api_token Application.get_env(:ex_aircall, :api_token)
-  @app_id Application.get_env(:ex_aircall, :app_id)
 
   @doc """
   Handy function to test api status or check for valid credentials
   """
   def ping do
-    url = "https://#{@app_id}:#{@api_token}@#{@base_url}/ping"
-    IO.inspect url
+    url = "https://#{app_id()}:#{api_token()}@#{@base_url}/ping"
     HTTPoison.get! url
   end
 
@@ -21,7 +18,11 @@ defmodule ExAircall.API do
   To include options, pass then as one string
   """
   def calls(opts \\ "") do
-    url = "https://#{@app_id}:#{@api_token}@#{@base_url}/calls#{opts}"
+    url = "https://#{app_id()}:#{api_token()}@#{@base_url}/calls#{opts}"
     HTTPoison.get! url
   end
+
+  defp api_token(), do: Application.get_env(:ex_aircall, :api_token)
+
+  defp app_id(), do: Application.get_env(:ex_aircall, :app_id)
 end
